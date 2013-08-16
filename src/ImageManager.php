a4d42b0785935b5b26ee47b5cbb501a9aa90b4b8
James Swift <swiftscripts@gmail.com>
2013-08-16 18:17:14 +0100

    Added support for last-modified and expires on Image

--- src/ImageManager.php
+++ src/ImageManager.php
@@ -1387,22 +1387,39 @@ class Image {
 	protected $_mime;
 	protected $_originalLocation;
 	protected $_allowedOutputFormats = array("image/jpeg","image/jp2","image/png","image/gif");
+	protected $_expires;
+	protected $_lastModified;
 	
 	//TODO: Add phpDoc
-	public function __construct($img){
+	public function __construct($img, $expires=null, $lastModified=null){
 		
+		//Check expires is valid
+		if (!ctype_digit($expires) && $expires!==null)
+			throw new Exception("Can't create new Image class. Please specify a valid value for \$expires (positive integer, or null).");
+			
+		//Check lastModified is valid
+		if (!ctype_digit($lastModified) && $lastModified!==null)
+			throw new Exception("Can't create new Image class. Please specify a valid value for \$lastModified (positive integer, or null).");
+			
 		//Load finfo to find the mime type of the passed file/string
 		$finfo = new \finfo(FILEINFO_MIME_TYPE);
 		
-		//If file was passed, load it into memory
+		//If a file reference was passed, load it into memory
 		if (is_file($img)){
+			
 			//Try to read mime data
 			$mime=$finfo->file($img);
 
+			//Load caching info
+			if ($lastModified===null)
+				$lastModified=\filemtime($img);
+			
 			//Load data
 			$this->_originalLocation=$img;
 			$img=file_get_contents($img);
+			
 		} else {
+			
 			//Try to read mime data from passed string
 			$mime=$finfo->buffer($img);
 		}
@@ -1416,6 +1433,8 @@ class Image {
 		//Populate data
 		$this->_img=$img;
 		$this->_mime=$mime;
+		$this->_expires=$expires;
+		$this->_lastModified=$lastModified;
 	}
 	
 	
@@ -1441,10 +1460,29 @@ class Image {
 		return strlen($this->getImageData());
 	}
 	
+	//TODO: Add phpDoc
 	public function getImageDimensions(){
 		$img = imagecreatefromstring($this->getImageData());
 		if ($img===false) return false;
 		return array("width"=>imagesx($img),"height"=>imagesy($img));
+	}
+	
+	//TODO: Add phpDoc
+	public function setLastModifiedHeader(){
+		if ($this->_lastModified===null)
+			return false;
+
+		header("Last-Modified: ".gmdate('D, d M Y H:i:s', $this->_lastModified));
+		return true;
+	}
+	
+	//TODO: Add phpDoc
+	public function setExpiresHeader($humanReadable=false){
+		if ($this->_expires===null || $this->_lastModified===null)
+			return false;
+		
+		header("Expires: ".gmdate('D, d M Y H:i:s', $this->_lastModified+$this->_expires));
+		return true;
 	}
 	
 	//TODO: Add phpDoc
@@ -1462,7 +1500,10 @@ class Image {
 				header($type.": ".$header);
 			}
 		}
+		
 		//Output caching variables
+		$this->setLastModifiedHeader();
+		$this->setExpiresHeader();
 		
 		//Output the image
 		header("Content-Type: ".$this->getMimeType());
